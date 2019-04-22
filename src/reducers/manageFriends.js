3532bd0601ecd3251c75d3c59ed1fc2563a1825d
Krystle Justine <KrystleJustine@gmail.com>
2019-04-22 16:52:55 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND' :
+      let newFriend = action.friend;
+      let newFriendArray = [...state.friends, newFriend];
+          return {...state, friends:newFriendArray};
+
+    case 'REMOVE_FRIEND' :
+      let id = action.id;
+      let filteredArray= state.friends.filter(friend => friend.id !== id);
+      return {...state, friends: filteredArray};
+
+    default:
+      return state
+  }
 }
